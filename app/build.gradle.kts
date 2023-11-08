0b56dbc26b13e8ce025c7b82a10ed963cf9c9f35
orxan405 <qurbanov_orxan@hotmail.com>
2023-11-08 19:38:32 +0400

    binding yaratdim

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.nexis.shopsmart"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nexis.shopsmart"
@@ -33,11 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/nexis/shopsmart/MainActivity.kt
+++ app/src/main/java/com/nexis/shopsmart/MainActivity.kt
@@ -2,10 +2,14 @@ package com.nexis.shopsmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nexis.shopsmart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
